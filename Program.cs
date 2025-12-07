cf2719ed5169674a5bcb571b7fb4df85c28542a1
Neli Hovanesyan <neli_hov@yahoo.com>
2025-12-07 13:50:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DictionaryCountryCapital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> capitals = new Dictionary<string, string>();
+            capitals["Bulgaria"] = "Sofia";
+            capitals["Malta"] = "La Valetta";
+            capitals["Spain"] = "Madrid";
+            capitals["Denmark"] = "Copenhagen";
+            capitals["Sweden"] = "Stockholm";
+            capitals["Norway"] = "Oslo";
+
+            foreach (var pair in capitals) { 
+            Console.WriteLine(pair.Key + " - " + pair.Value);
+            }
+
+        }
+    }
+}
